70c020f01b29e3e6200c04985a4470c1fef91297
Artur Ostrega <artur@ostrega.me>
2013-04-21 05:45:05 -0500

    README, comments, tweaks

---
+++ README.md
@@ -0,0 +1,7 @@
+This is my resume. The data is JSON, the template is Handlebars, the styling is CSS, and it's rendered into a PDF using PhantomJS.
+
+## Usage
+```
+npm install
+./build.js
+```
--- build.js
+++ build.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env phantomjs
+# build.js - Build PDF from a Handlebars and JSON file.
+# Public domain
 
+# Required modules
 var fs = require('fs');
 var handlebars = require('handlebars');
 
+# Put the data in the template then produce a temporary HTML file.
 var data = require('./data.json');
-var templateSource = fs.read('template.html.handlebars');
-
+var templateSource = fs.read('template.handlebars');
 var template = handlebars.compile(templateSource);
 var html = template(data);
 fs.write('_resume.html', html, 'w');
 
+# Render the HTML file into a PDF then delete the HTML file and exit.
 var page = require('webpage').create();
 page.paperSize = { format: 'Letter', orientation: 'portrait', border: '.5in' }
 page.open('_resume.html', function (status) {
@@ -22,6 +26,3 @@ page.open('_resume.html', function (status) {
   fs.remove('_resume.html');
   phantom.exit();
 });
-
-//fs.writeFileSync('resume.html', resume);
-//console.log("Built resume to resume.html.");
