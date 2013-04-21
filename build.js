55db64ce36faaef3fca60758c12457222997690a
Artur Ostrega <artur@ostrega.me>
2013-04-21 05:24:11 -0500

    unified build script, tweaked resume, added .gitignore

--- build.js
+++ build.js
@@ -1,12 +1,27 @@
-#!/usr/bin/env node
+#!/usr/bin/env phantomjs
 
 var fs = require('fs');
 var handlebars = require('handlebars');
 
 var data = require('./data.json');
-var templateSource = fs.readFileSync('template.html.handlebars', 'utf-8');
+var templateSource = fs.read('template.html.handlebars');
 
 var template = handlebars.compile(templateSource);
-var resume = template(data);
-fs.writeFileSync('resume.html', resume);
-console.log("Built resume to resume.html.");
+var html = template(data);
+fs.write('_resume.html', html, 'w');
+
+var page = require('webpage').create();
+page.paperSize = { format: 'Letter', orientation: 'portrait', border: '.5in' }
+page.open('_resume.html', function (status) {
+  if (status === 'success') {
+    page.render('resume.pdf');
+    console.log("Built to resume.pdf");
+  } else {
+    console.log("Error: Unable to open page");
+  }
+  fs.remove('_resume.html');
+  phantom.exit();
+});
+
+//fs.writeFileSync('resume.html', resume);
+//console.log("Built resume to resume.html.");
