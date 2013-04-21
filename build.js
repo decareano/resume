37e6d3bed2c247dcd139b02ada877dc565c21ced
Artur Ostrega <artur@ostrega.me>
2013-04-21 05:49:45 -0500

    this is not coffee

--- build.js
+++ build.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env phantomjs
-# build.js - Build PDF from a Handlebars and JSON file.
-# Public domain
+// build.js - Build PDF from a Handlebars and JSON file.
+// Public domain
 
-# Required modules
+// Required modules
 var fs = require('fs');
 var handlebars = require('handlebars');
 
-# Put the data in the template then produce a temporary HTML file.
+// Put the data in the template then produce a temporary HTML file.
 var data = require('./data.json');
 var templateSource = fs.read('template.handlebars');
 var template = handlebars.compile(templateSource);
 var html = template(data);
 fs.write('_resume.html', html, 'w');
 
-# Render the HTML file into a PDF then delete the HTML file and exit.
+// Render the HTML file into a PDF then delete the HTML file and exit.
 var page = require('webpage').create();
 page.paperSize = { format: 'Letter', orientation: 'portrait', border: '.5in' }
 page.open('_resume.html', function (status) {
